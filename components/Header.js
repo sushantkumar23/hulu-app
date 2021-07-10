8188de1065975ae9997ef3575a83aa73f0843e59
Sushant Kumar <sushantkumar23@gmail.com>
2021-07-10 23:47:34 +0530

    Adding the hulu logo to the app

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
+import Image from 'next/image'
+
 function Header() {
   return (
     <header>
       <h1>This is the header</h1>
+      <Image
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      ></Image>
     </header>
   )
 }
